a36fcdbe17be1767b6ea5a8b06fdcf2ac1cf7da3
Filbert <filberttt@hotmail.com>
2019-08-26 19:38:41 +0700

    another fix

--- codehacks/src/makeComponent.ts
+++ codehacks/src/makeComponent.ts
@@ -18,7 +18,7 @@ async function makeComponent(editor: vscode.TextEditor, input: string) {
     while (match = logRegex.exec(documentText)) {
         let matchRange = new vscode.Range(document.positionAt(match.index), document.positionAt(match.index + match[0].length));
         if (!matchRange.isEmpty) {
-            importStatements.push(document.getText(matchRange));
+            importStatements.push(matchRange);
         }
     }
     if(importStatements.length === 0) {
